819732fe1cd8d7a27d3ef75e90a047b153df4a35
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-21 19:01:06 -0300

    solves exercise 4) git add . git status

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-2-array-and-loop-for/exercises.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-2-array-and-loop-for/exercises.js
@@ -1,26 +1,39 @@
-// Exercício 1
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// // Exercício 1
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index=0; index<numbers.length ; index += 1) {
-    console.log(numbers[index]);
-}
-console.log (numbers.length);
+// for (let index=0; index<numbers.length ; index += 1) {
+//     console.log(numbers[index]);
+// }
+// console.log (numbers.length);
 
-//Exercício 2
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// //Exercício 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let sum =0;
-for (let index=0; index<numbers.length ; index += 1) {
-    sum +=numbers[index]
-}
-console.log(sum);
+// let sum =0;
+// for (let index=0; index<numbers.length ; index += 1) {
+//     sum +=numbers[index]
+// }
+// console.log(sum);
 
-//Exercício 3
+// //Exercício 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum =0;
+// for (let index=0; index<numbers.length ; index += 1) {
+//     sum +=numbers[index];
+// }
+//     let avg = sum/numbers.length;
+//     console.log(avg);
+
+// //Exercício 4
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum =0;
 for (let index=0; index<numbers.length ; index += 1) {
     sum +=numbers[index];
 }
     let avg = sum/numbers.length;
-    console.log(avg);
-
+    if (avg>20) {
+        console.log('valor maior que 20');
+    }
+    else {
+        console.log('valor menor ou igual a 20');
+    }
