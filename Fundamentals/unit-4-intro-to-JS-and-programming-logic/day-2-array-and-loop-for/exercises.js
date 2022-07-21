0e75f09a91cc5c9d9708b7c5ec218c6146d1bceb
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-21 19:12:04 -0300

    solves exercise 6

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-2-array-and-loop-for/exercises.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-2-array-and-loop-for/exercises.js
@@ -25,15 +25,46 @@
 //     console.log(avg);
 
 // //Exercício 4
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum =0;
+// for (let index=0; index<numbers.length ; index += 1) {
+//     sum +=numbers[index];
+// }
+//     let avg = sum/numbers.length;
+//     if (avg>20) {
+//         console.log('valor maior que 20');
+//     }
+//     else {
+//         console.log('valor menor ou igual a 20');
+//     }
+
+//Exercício 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let higherNumber = numbers[0];
+
+// for (let index=1; index<numbers.length;index+=1) {
+
+// if (numbers[index]>higherNumber) {
+//     higherNumber = numbers[index];
+// }
+// }
+// console.log(higherNumber);
+
+//Exercício 6
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum =0;
-for (let index=0; index<numbers.length ; index += 1) {
-    sum +=numbers[index];
-}
-    let avg = sum/numbers.length;
-    if (avg>20) {
-        console.log('valor maior que 20');
+let oddNumbersCount=0;
+
+for(index=0; index<numbers.length;index+=1) {
+    if (numbers[index]%2 !== 0){
+        oddNumbersCount+=1;
     }
+}
+if (oddNumbersCount === 0) {
+    console.log ("nenhum valor ímpar encontrado")
+    
+}
     else {
-        console.log('valor menor ou igual a 20');
+        console.log(oddNumbersCount);
+
     }
