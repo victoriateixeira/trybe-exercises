596899126e2298e76b8dd83a0f9fbe25ed437aec
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-22 18:39:44 -0300

    solves exercise 3

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-3-programming-logic-and-algorithms/exercises4.3.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-3-programming-logic-and-algorithms/exercises4.3.js
@@ -9,10 +9,49 @@
 
 //Exercício 2
 
-let word = 'victoria';
-let wordBackwards = "";
+// let word = 'victoria';
+// let wordBackwards = "";
 
-for (i=word.length-1;i>=0;i-=1) {
-wordBackwards+= word[i];
+// for (i=word.length-1;i>=0;i-=1) {
+// wordBackwards+= word[i];
+// }
+// console.log (wordBackwards);
+
+//Exercício 3
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for ( let n=0;n<numbers.length-1;n+=1) {
+   
+// for (let index = 1+n; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] > numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   }
+// // }
+
+// let words = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let longestWord= words[0];
+
+// for (i=1;i<words.length;i+=1) {
+//     if (words[i].length>longestWord.length) {
+//         longestWord = words[i];
+//     }
+// }
+// console.log (longestWord);
+
+
+let words = ['java', 'javascript', 'python', 'html', 'css'];
+
+let shortestWord= words[0];
+
+for (i=1;i<words.length;i+=1) {
+    if (words[i].length<shortestWord.length) {
+        shortestWord = words[i];
+    }
 }
-console.log (wordBackwards);
+console.log (shortestWord);
