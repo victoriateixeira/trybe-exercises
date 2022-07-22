18e104746e5a1a9f63b4d92d89dfd303077ae48c
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-22 18:30:37 -0300

    solves exercise 2

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-3-programming-logic-and-algorithms/exercises4.3.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-3-programming-logic-and-algorithms/exercises4.3.js
@@ -1,8 +1,18 @@
 //Exercício 1
 
-let number=10;
-let fatorial=number;
-for (i=number-1;i>0;i-=1) {
-fatorial=fatorial*i;
+// let number=10;
+// let fatorial=number;
+// for (i=number-1;i>0;i-=1) {
+// fatorial=fatorial*i;
+// }
+// console.log (fatorial);
+
+//Exercício 2
+
+let word = 'victoria';
+let wordBackwards = "";
+
+for (i=word.length-1;i>=0;i-=1) {
+wordBackwards+= word[i];
 }
-console.log (fatorial);
+console.log (wordBackwards);
