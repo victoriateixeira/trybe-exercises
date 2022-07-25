a7a221c6f2a63f113bba2ca8f845c864fefb9ee2
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 18:57:00 -0300

    solves exercise 5

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -30,13 +30,37 @@
 //   }
 
 //Exercício 4
-let info = {
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
+
+//   for (key in info) {
+//     console.log (info[key]);
+//   }
+
+//Exercício 5
+
+let infoMargarida = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'Sim',
 };
+let infoTioPatinhas = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MAcPatinhas",
+  recorrente: 'Sim',
+};
 
-  for (key in info) {
-    console.log (info[key]);
-  }
+for ( let key in infoMargarida) {
+    if (key === "recorrente" && infoMargarida.recorrente==="Sim" && infoTioPatinhas.recorrente ==="Sim") {
+console.log ("Ambos recorrentes")
+    }
+    else {
+        console.log(infoMargarida[key],'e', infoTioPatinhas[key]);
+    }
+}
