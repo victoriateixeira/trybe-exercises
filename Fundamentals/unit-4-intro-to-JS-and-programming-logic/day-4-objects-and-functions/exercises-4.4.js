673d3a56e70a473c8c44669caa1e79a481dd8f03
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 19:03:36 -0300

    solves exercise 7

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -79,3 +79,23 @@
 //     ],
 //   };
 //   console.log ('O livro favorito de', leitor.nome,leitor.sobrenome, 'se chama',leitor.livrosFavoritos[0].titulo);
+
+//Exercício 7
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      },
+    ],
+  };
+  console.log (leitor);
