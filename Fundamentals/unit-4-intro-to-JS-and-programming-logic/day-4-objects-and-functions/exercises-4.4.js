aa87a22b6855d5171da61168ba7fcca6307f7fe8
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 19:00:17 -0300

    solves exercise 6

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -43,24 +43,39 @@
 
 //Exercício 5
 
-let infoMargarida = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
-};
-let infoTioPatinhas = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: "O último MAcPatinhas",
-  recorrente: 'Sim',
-};
+// let infoMargarida = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
+// let infoTioPatinhas = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: "O último MAcPatinhas",
+//   recorrente: 'Sim',
+// };
 
-for ( let key in infoMargarida) {
-    if (key === "recorrente" && infoMargarida.recorrente==="Sim" && infoTioPatinhas.recorrente ==="Sim") {
-console.log ("Ambos recorrentes")
-    }
-    else {
-        console.log(infoMargarida[key],'e', infoTioPatinhas[key]);
-    }
-}
+// for ( let key in infoMargarida) {
+//     if (key === "recorrente" && infoMargarida.recorrente==="Sim" && infoTioPatinhas.recorrente ==="Sim") {
+// console.log ("Ambos recorrentes")
+//     }
+//     else {
+//         console.log(infoMargarida[key],'e', infoTioPatinhas[key]);
+//     }
+// }
+
+//Exercício 6
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+//   console.log ('O livro favorito de', leitor.nome,leitor.sobrenome, 'se chama',leitor.livrosFavoritos[0].titulo);
