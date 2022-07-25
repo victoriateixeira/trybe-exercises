c3b79c4a683c85d0a931373dc66b3f3f150448d8
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 18:40:10 -0300

    creates exercise file

---
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -0,0 +1,18 @@
+// //Exercício 1
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+//   console.log ('Bem vinda,', info.personagem);
+
+//Exercício 2
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info.recorrente = "Sim";
+  console.log(info);
