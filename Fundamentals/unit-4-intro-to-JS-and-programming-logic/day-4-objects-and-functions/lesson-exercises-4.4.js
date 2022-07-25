97a3ea2db0c6600d77e4c845ea5b5ef64ac20a81
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 18:38:01 -0300

    solves exercise 1

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/lesson-exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/lesson-exercises-4.4.js
@@ -38,12 +38,12 @@
 //   }
 
 //2
-let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
 
-  for (key in car) {
-    console.log (key,car[key]);
-  };
+//   for (key in car) {
+//     console.log (key,car[key]);
+//   };
