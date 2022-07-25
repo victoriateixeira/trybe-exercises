d76466f78d8fef2a5ec24f06aa03898fc49babe7
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 18:42:40 -0300

    solves exercise 3

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -8,11 +8,23 @@
 
 //Exercício 2
 
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info.recorrente = "Sim";
+//   console.log(info);
+
+//Exercício 3
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
   };
 
-  info.recorrente = "Sim";
-  console.log(info);
+  for (key in info) {
+    console.log (key,':',info[key]);
+  }
