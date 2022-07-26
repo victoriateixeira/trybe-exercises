a365b5e44864d55b81600a203a9cfe51db772504
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-07-25 22:36:00 -0300

    solves functions exercise 6

--- Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
+++ Fundamentals/unit-4-intro-to-JS-and-programming-logic/day-4-objects-and-functions/exercises-4.4.js
@@ -160,25 +160,35 @@
 // console.log(findLargestString(testArray));
 
 //Exercício 5
-let testArray = [2, 3, 2, 5, 8, 2, 3, 6, 6, 6, 6,7,7,7,7,7,3,3,3,3,3,3];
-let count=0;
-let timesRepeated = 0;
-let mostRepeatedNumber=0;
-function findsMostRepeatedNumber (array) {
-    for (index=0;index<array.length;index+=1) {
-        for (index2=0;index2<array.length;index2+=1) {
-            if (array[index]=== array[index2]){
-                count+=1;
+// let testArray = [2, 3, 2, 5, 8, 2, 3, 6, 6, 6, 6,7,7,7,7,7,3,3,3,3,3,3];
+// let count=0;
+// let timesRepeated = 0;
+// let mostRepeatedNumber=0;
+// function findsMostRepeatedNumber (array) {
+//     for (index=0;index<array.length;index+=1) {
+//         for (index2=0;index2<array.length;index2+=1) {
+//             if (array[index]=== array[index2]){
+//                 count+=1;
             
-            if (count>timesRepeated){
-                timesRepeated = count;
-                mostRepeatedNumber=array[index];
-            }
-        }
+//             if (count>timesRepeated){
+//                 timesRepeated = count;
+//                 mostRepeatedNumber=array[index];
+//             }
+//         }
         
-        }
-        count=0;
+//         }
+//         count=0;
+//     }
+//     return mostRepeatedNumber;
+// }
+// console.log (findsMostRepeatedNumber(testArray));
+
+//Exercício 6
+let sum=0;
+function sumsNumbersUntilN(number) {
+    for (n=number; n>0;n-=1) {
+        sum +=n;
     }
-    return mostRepeatedNumber;
+    return sum;
 }
-console.log (findsMostRepeatedNumber(testArray));
+console.log(sumsNumbersUntilN(10));
