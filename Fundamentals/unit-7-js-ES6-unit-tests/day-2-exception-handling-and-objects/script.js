a69ddddd7bec680a58d68846bc1cc39165238480
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-15 19:11:14 -0300

    resolve Parte 1- Exercicio 1

---
+++ Fundamentals/unit-7-js-ES6-unit-tests/day-2-exception-handling-and-objects/script.js
@@ -0,0 +1,40 @@
+// //Exercício 1 [adicionando novas chaves]
+
+// const addsNewEntry = (object, key, value) => {
+// object[key] = value;
+// return object;
+// }
+// let customer = {
+// name: 'Victoria', 
+// age : 27,
+// birthPlace: 'Belo Horizonte',
+// }
+
+// console.log(addsNewEntry(customer, 'country', 'Brazil'));
+
+// //Exercício 2 [object.keys]
+
+// const displaysObjectEntries = (object) => {
+
+//     for (let key in object) {
+//   console.log(`${key}, Nível:${object[key]}`)
+//     }
+// }
+
+// const student1 = {
+//     Html: 'Muito Bom',
+//     Css: 'Bom',
+//     JavaScript: 'Ótimo',
+//     SoftSkills: 'Ótimo',
+//   };
+//   const student2 = {
+//     Html: 'Bom',
+//     Css: 'Ótimo',
+//     JavaScript: 'Ruim',
+//     SoftSkills: 'Ótimo',
+//     Git: 'Bom', // chave adicionada
+//   };
+// console.log (displaysObjectEntries (student1));
+
+//PARTE 1 - Exercício 1
+
