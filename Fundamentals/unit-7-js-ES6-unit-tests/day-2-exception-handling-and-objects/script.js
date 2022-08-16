1ca3852855a9f838e29d99e5f335c9f31e3fc36c
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-08-16 19:13:56 -0300

    resolvendo os exercícios do dia

--- Fundamentals/unit-7-js-ES6-unit-tests/day-2-exception-handling-and-objects/script.js
+++ Fundamentals/unit-7-js-ES6-unit-tests/day-2-exception-handling-and-objects/script.js
@@ -36,5 +36,109 @@
 //   };
 // console.log (displaysObjectEntries (student1));
 
-//PARTE 1 - Exercício 1
+//PARTE 2 - Exercício 1
 
+// const order = {
+//     name: 'Rafael Andrade',
+//     phoneNumber: '11-98763-1416',
+//     address: {
+//       street: 'Rua das Flores',
+//       number: '389',
+//       apartment: '701',
+//     },
+//     order: {
+//       pizza: {
+//         marguerita: {
+//           amount: 1,
+//           price: 25,
+//         },
+//         pepperoni: {
+//           amount: 1,
+//           price: 20,
+//         }
+//       },
+//       drinks: {
+//         coke: {
+//           type: 'Coca-Cola Zero',
+//           price: 10,
+//           amount: 1,
+//         }
+//       },
+//       delivery: {
+//         deliveryPerson: 'Ana Silveira',
+//         price: 5,
+//       }
+//     },
+//     payment: {
+//       total: 60,
+//     },
+//   };
+  
+//   const customerInfo = (order) => {
+//     return `Olá ${order.order.delivery.deliveryPerson}, entrega para ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, N˚${order.address.number}, AP: ${order.address.apartment}`;
+  
+//   }
+  
+//   console.log(customerInfo(order));
+  
+//   const orderModifier = (order) => {
+//     // Adicione abaixo as informações necessárias.
+//     const newBuyer = order.name = 'Luiz Silva';
+//     const newTotal = order.payment.total = '50';
+//     const pizzas = Object.keys(order.order.pizza);
+//     const drinks = order.order.drinks.coke.type;
+//     console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${newTotal},00.`);
+   
+//     };
+    
+  
+//   orderModifier(order);
+
+
+//PARTE 3 - Exercício 1
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+//   const changesLesson2Shift = (object,key,value) => {
+//     object[key] = value;
+//     return object;
+//   };
+
+
+//   const listsObjectsKeys = (object) => {
+//     const objectKeys = Object.keys(object);
+//     return objectKeys;
+//   };
+
+//   const findsObjectSize = (object) => {
+//     const objectEntries = Object.entries(object);
+//     return objectEntries.length;
+//   };
+
+//   const listsObjectsValues = (object) => {
+//     const objectValues = Object.values(object);
+//     return objectValues;
+//   };
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+console.log(allLessons);
+  
